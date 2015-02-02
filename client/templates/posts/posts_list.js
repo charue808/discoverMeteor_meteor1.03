658a535242c504a04e19298b9ee6b082681cedac
charue808 <charlesgum@gmail.com>
2015-02-02 02:48:21 +0000

    completed chapter 13

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,7 @@
+//Removed postList helper Set the data context at route level
+/*
+Template.postsList.helpers({
+    posts: function() {
+        return Posts.find({}, {sort: {submitted: -1}});
+    }
+});*/
