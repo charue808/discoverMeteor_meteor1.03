c1064387e97ac7d17b9c34576a217e6dd89eb2ef
charue808 <charlesgum@gmail.com>
2015-02-02 04:15:13 +0000

    animations chapter 14

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,7 +1,8 @@
-//Removed postList helper Set the data context at route level
-/*
 Template.postsList.helpers({
-    posts: function() {
-        return Posts.find({}, {sort: {submitted: -1}});
+    postsWithRank: function() {
+        return this.posts.map(function(post, index, cursor) {
+            post._rank = index;
+            return post;
+        });
     }
-});*/
+});
